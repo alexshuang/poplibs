f16f53017842782f6dd33cb3f283c37e3068c616
Huang,Shuang <alexhu@graphcore.ai>
2023-04-05 21:36:36 -0900

    dump hostOut and save it to hostout.txt

--- tools/static_sparse_matmul.cpp
+++ tools/static_sparse_matmul.cpp
@@ -57,6 +57,24 @@ using namespace popsparse;
 using namespace popsparse::dynamic;
 using namespace poputil;
 
+template <typename T>
+static void dump_output(T *begin, T* end, const std::string& filename) {
+  std::ofstream cout(filename);
+  if (!cout.is_open()) {
+    std::cerr << "Cannot open hostOut file " << filename << "\n";
+    assert(0);
+  }
+
+  std::cerr << "hostOut: ";
+  for (auto it = begin; it != end; ++it) {
+    std::cerr << *it << " ";
+    cout << *it << " ";
+  }
+  std::cerr << std::endl;
+  cout.close();
+  std::cerr << "Save hostOut to " << filename << std::endl;
+}
+
 template <typename T, typename F>
 void writeValues(T *begin, T *end, F generator) {
   for (auto it = begin; it != end; ++it) {
@@ -356,7 +374,7 @@ int main(int argc, char **argv) try {
   const auto weightFileUsed = !weightFileName.empty();
   const auto inputFileUsed = !inputFileName.empty();
 
-  if (maskFileUsed) {
+  if (maskFileUsed && !weightFileUsed) {
     if (!readHeaderFromMaskFile(sparsityFileName, m, k, blockLength)) {
       return 1;
     }
@@ -518,7 +536,7 @@ int main(int argc, char **argv) try {
                                        debugString, sparseOptionFlags, &cache);
 
   prog.add(Repeat(iters, repeat_prog));
-  // prog.add(PrintTensor(out));
+  prog.add(PrintTensor("sparse matmul output", out));
   std::cerr << "\nhostOut shape: [" << out.dim(0) << ", " << out.dim(1) << ", "
             << out.dim(2) << "]";
 
@@ -567,6 +585,12 @@ int main(int argc, char **argv) try {
     writeDenseValues<double>(target, dataType, hostDense.data(),
                             hostDense.data() + hostDense.num_elements(),
                             inputFileName);
+    std::cerr << "\ninput: ";
+    for (auto it = hostDense.data();
+         it != hostDense.data() + hostDense.num_elements(); it++) {
+      std::cerr << *it << " ";
+    }
+    std::cerr << std::endl;
   } else {
     if (useBipolarDistribution) {
       writeRandomBinaryValues(target, dataType, hostDense, -1.0, 1.0,
@@ -638,6 +662,8 @@ int main(int argc, char **argv) try {
       poplibs_test::gemm::generalMatrixMultiply(hostDenseLHS, hostDense,
                                                 modelOut, false, false);
     }
+    dump_output(hostOut.data(), hostOut.data() + hostOut.num_elements(),
+                "hostout.txt");
     matchesModel &= checkIsClose("out", hostOut, modelOut, relTolerance);
   }
 
